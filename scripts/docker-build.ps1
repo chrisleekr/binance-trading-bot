78c7e950ac4a85902c58c83b3d869f90d3b6942d
garyng <garyng.zhongbo@gmail.com>
2021-05-14 19:10:55 +0800

    Use switch instead

--- scripts/docker-build.ps1
+++ scripts/docker-build.ps1
@@ -8,12 +8,16 @@ param (
 
 $packageVersion = $(node -p "require('./package.json').version")
 $gitHash = $(git rev-parse --short HEAD)
-$nodeEnv = "production"
-$target = "production-stage"
 
-if ($env -eq "dev") {
-  $nodeEnv = "development"
-  $target = "dev-stage"
+switch ($env) {
+  "prod" {
+    $nodeEnv = "production"
+    $target = "production-stage"
+  }
+  "dev" {
+    $nodeEnv = "development"
+    $target = "dev-stage"
+  }
 }
 
 docker build . --build-arg PACKAGE_VERSION=$packageVersion --build-arg GIT_HASH=$gitHash --build-arg NODE_ENV=$nodeEnv --target $target -t chrisleekr/binance-trading-bot:latest
