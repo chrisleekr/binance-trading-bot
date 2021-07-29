f4f0a4695251770e1feb67c6d030856641a13346
Etienne BLANC <etienne.blanc94@gmail.com>
2021-07-29 21:31:45 +0200

    ⚡️ improve password handling

--- app/frontend/websocket/handlers/handle-password.js
+++ app/frontend/websocket/handlers/handle-password.js
@@ -1,28 +1,42 @@
 const config = require('config');
+const bcrypt = require('bcrypt');
 const { PubSub, cache } = require('../../../helpers');
 
 const handlePassword = async (logger, ws, payload) => {
   logger.info({ payload }, 'Start password verify');
+  ws.send(
+    JSON.stringify({
+      result: true,
+      type: 'debug',
+      content: payload
+    })
+  );
 
-  const { data: typedPassword } = payload;
+  const {
+    data: { password, loginWindowMinutes }
+  } = payload;
+  const retrievedPassword = config.get('password'); // The valid password
 
-  const retrievedPassword = config.get('password');
-
-  if (retrievedPassword === typedPassword.pass) {
-    typedPassword.config.botOptions.login.logged = true;
-    typedPassword.config.botOptions.login.elapsedTime = new Date();
-    await cache.set(
-      `tempLogin`,
-      JSON.stringify(typedPassword.config.botOptions.login),
-      typedPassword.config.botOptions.login.loginWindowMinutes * 60
-    );
-
-    PubSub.publish('frontend-notification', {
-      type: 'success',
-      title: `Unlocking bot.`
+  // If wrong password, send an error notification:
+  if (retrievedPassword !== password) {
+    return PubSub.publish('frontend-notification', {
+      type: 'error',
+      title: `Bad password.`
     });
-    ws.send(JSON.stringify({ result: true, type: 'login-success' }));
   }
+  ws.send(JSON.stringify({ conf: config }));
+  // Update cache to auto-auth:
+  await cache.set(
+    `tempLogin`,
+    JSON.stringify({ logged: true, elapsedTime: new Date() }),
+    loginWindowMinutes * 60
+  );
+
+  PubSub.publish('frontend-notification', {
+    type: 'success',
+    title: `Unlocking bot.`
+  });
+  ws.send(JSON.stringify({ result: true, type: 'login-success' }));
 };
 
 module.exports = { handlePassword };
