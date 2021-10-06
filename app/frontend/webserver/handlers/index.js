06eb81b1afe52caf62cdc1f9f0f0dba1b2b3934b
Alessandro Marchioro <marciozgaming@gmail.com>
2021-10-06 11:17:18 +0200

    feat: added status basic endpoint (#347)

--- app/frontend/webserver/handlers/index.js
+++ app/frontend/webserver/handlers/index.js
@@ -3,12 +3,14 @@ const { handleGridTradeArchiveGet } = require('./grid-trade-archive-get');
 const { handleGridTradeArchiveDelete } = require('./grid-trade-archive-delete');
 const { handleClosedTradesSetPeriod } = require('./closed-trades-set-period');
 const { handle404 } = require('./404');
+const { handleStatus } = require('./status');
 
 const setHandlers = async (logger, app, { loginLimiter }) => {
   await handleAuth(logger, app, { loginLimiter });
   await handleGridTradeArchiveGet(logger, app);
   await handleGridTradeArchiveDelete(logger, app);
   await handleClosedTradesSetPeriod(logger, app);
+  await handleStatus(logger, app);
   await handle404(logger, app);
 };
 
