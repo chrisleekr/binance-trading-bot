2ad3c71bd94d55a4b5ba0b5ec985e70bef7de6fd
Alessandro Marchioro <marciozgaming@gmail.com>
2021-10-11 12:30:51 +0200

    feat: account balance quote estimates for bot's currently handling assets (#353)

---
+++ app/frontend/webserver/handlers/__tests__/status.test.js
@@ -0,0 +1,26 @@
+/* eslint-disable global-require */
+describe('webserver/handlers/status', () => {
+  const appMock = {};
+
+  let resSendMock;
+
+  beforeEach(async () => {
+    resSendMock = jest.fn().mockResolvedValue(true);
+    appMock.get = jest.fn().mockImplementation((_path, func) => {
+      func(null, { send: resSendMock });
+    });
+    const loggerMock = require('../../../../helpers/logger');
+
+    const { handleStatus } = require('../status');
+
+    await handleStatus(loggerMock, appMock);
+  });
+
+  it('triggers res.send', () => {
+    expect(resSendMock).toHaveBeenCalledWith({
+      success: true,
+      status: 200,
+      message: 'OK'
+    });
+  });
+});
