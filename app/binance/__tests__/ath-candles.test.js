dea22c10424b75803aa87bc01d6e82340be57d26
Habib Alkhabbaz <31035020+habibalkhabbaz@users.noreply.github.com>
2022-07-18 00:53:46 +0300

    test: add test for candles & ath-candles

---
+++ app/binance/__tests__/ath-candles.test.js
@@ -0,0 +1,365 @@
+/* eslint-disable global-require */
+describe('ath-candles.js', () => {
+  let athCandles;
+  let binanceMock;
+  let loggerMock;
+  let mongoMock;
+
+  let mockGetConfiguration;
+  let mockSaveCandle;
+
+  let mockWebsocketATHCandlesClean;
+
+  beforeEach(() => {
+    jest.clearAllMocks().resetModules();
+  });
+
+  describe('when ATH enabled', () => {
+    describe('setupATHCandlesWebsocket', () => {
+      beforeEach(async () => {
+        const { binance, logger } = require('../../helpers');
+        binanceMock = binance;
+        loggerMock = logger;
+
+        mockGetConfiguration = jest
+          .fn()
+          .mockImplementation((_logger, _symbol) => ({
+            buy: {
+              athRestriction: {
+                enabled: true,
+                candles: { interval: '1d' }
+              }
+            }
+          }));
+
+        mockSaveCandle = jest.fn().mockResolvedValue(true);
+
+        mockWebsocketATHCandlesClean = jest.fn();
+
+        binanceMock.client.ws.candles = jest.fn(
+          (symbols, candleInterval, fn) => {
+            fn({
+              eventType: 'kline',
+              eventTime: 1525285576276,
+              symbol: 'ETHBTC',
+              startTime: 1525285560000,
+              open: '0.04898000',
+              high: '0.04902700',
+              low: '0.04898000',
+              close: '0.04901900',
+              closeTime: 1525285619999,
+              volume: '37.89600000',
+              trades: 30,
+              interval: '30m',
+              isFinal: false,
+              quoteVolume: '1.85728874',
+              buyVolume: '21.79900000',
+              quoteBuyVolume: '1.06838790'
+            });
+
+            return mockWebsocketATHCandlesClean;
+          }
+        );
+
+        jest.mock('../../cronjob/trailingTradeHelper/common', () => ({
+          saveCandle: mockSaveCandle
+        }));
+
+        jest.mock('../../cronjob/trailingTradeHelper/configuration', () => ({
+          getConfiguration: mockGetConfiguration
+        }));
+
+        athCandles = require('../ath-candles');
+
+        await athCandles.setupATHCandlesWebsocket(loggerMock, [
+          'ETHBTC',
+          'BNBUSDT'
+        ]);
+      });
+
+      it('triggers getConfiguration twice', () => {
+        expect(mockGetConfiguration).toHaveBeenCalledTimes(2);
+      });
+
+      it('triggers getConfiguration for ETHBTC', () => {
+        expect(mockGetConfiguration).toHaveBeenCalledWith(loggerMock, 'ETHBTC');
+      });
+
+      it('triggers getConfiguration for BNBUSDT', () => {
+        expect(mockGetConfiguration).toHaveBeenCalledWith(
+          loggerMock,
+          'BNBUSDT'
+        );
+      });
+
+      it('triggers saveCandle', () => {
+        expect(mockSaveCandle).toHaveBeenCalledWith(
+          loggerMock,
+          'trailing-trade-ath-candles',
+          {
+            close: 0.049019,
+            high: 0.049027,
+            interval: '30m',
+            key: 'ETHBTC',
+            low: 0.04898,
+            open: 0.04898,
+            time: 1525285560000,
+            volume: 37.896
+          }
+        );
+      });
+
+      it('checks websocketATHCandlesClean', () => {
+        expect(athCandles.getWebsocketATHCandlesClean()).toStrictEqual({
+          '1d': expect.any(Function)
+        });
+      });
+
+      it('does not trigger websocketATHCandlesClean', () => {
+        expect(mockWebsocketATHCandlesClean).not.toHaveBeenCalled();
+      });
+
+      describe('when called again', () => {
+        beforeEach(async () => {
+          mockGetConfiguration.mockClear();
+          await athCandles.setupATHCandlesWebsocket(loggerMock, ['BTCUSDT']);
+        });
+
+        it('triggers getConfiguration twice', () => {
+          expect(mockGetConfiguration).toHaveBeenCalledTimes(1);
+        });
+
+        it('triggers getConfiguration for BTCUSDT', () => {
+          expect(mockGetConfiguration).toHaveBeenCalledWith(
+            loggerMock,
+            'BTCUSDT'
+          );
+        });
+
+        it('triggers websocketATHCandlesClean', () => {
+          expect(mockWebsocketATHCandlesClean).toHaveBeenCalledTimes(1);
+        });
+      });
+    });
+    describe('syncATHCandles', () => {
+      beforeEach(async () => {
+        const { binance, logger, mongo } = require('../../helpers');
+        binanceMock = binance;
+        loggerMock = logger;
+        mongoMock = mongo;
+
+        mockGetConfiguration = jest
+          .fn()
+          .mockImplementation((_logger, _symbol) => ({
+            buy: {
+              athRestriction: {
+                enabled: true,
+                candles: { interval: '1d', limit: 1 }
+              }
+            }
+          }));
+
+        mockSaveCandle = jest.fn().mockResolvedValue(true);
+
+        mongoMock.deleteAll = jest.fn().mockResolvedValue(true);
+
+        binanceMock.client.candles = jest.fn().mockImplementation(options => {
+          const { symbol } = options;
+
+          if (symbol === 'ETHBTC') {
+            return [
+              {
+                openTime: 1508328900000,
+                open: '0.05655000',
+                high: '0.05656500',
+                low: '0.05613200',
+                close: '0.05632400',
+                volume: '68.88800000',
+                closeTime: 1508329199999,
+                quoteAssetVolume: '2.29500857',
+                trades: 85,
+                baseAssetVolume: '40.61900000'
+              }
+            ];
+          }
+
+          return [];
+        });
+
+        jest.mock('../../cronjob/trailingTradeHelper/common', () => ({
+          saveCandle: mockSaveCandle
+        }));
+
+        jest.mock('../../cronjob/trailingTradeHelper/configuration', () => ({
+          getConfiguration: mockGetConfiguration
+        }));
+
+        athCandles = require('../ath-candles');
+
+        await athCandles.syncATHCandles(loggerMock, ['ETHBTC']);
+      });
+
+      it('triggers mongo.deleteAll', () => {
+        expect(mongoMock.deleteAll).toHaveBeenCalledWith(
+          loggerMock,
+          'trailing-trade-ath-candles',
+          {
+            key: 'ETHBTC'
+          }
+        );
+      });
+
+      it('triggers getConfiguration for ETHBTC', () => {
+        expect(mockGetConfiguration).toHaveBeenCalledWith(loggerMock, 'ETHBTC');
+      });
+
+      it('triggers binance.client.candles', () => {
+        expect(binanceMock.client.candles).toHaveBeenCalledWith({
+          interval: '1d',
+          limit: 1,
+          symbol: 'ETHBTC'
+        });
+      });
+
+      it('triggers saveCandle one time', () => {
+        expect(mockSaveCandle).toHaveBeenCalledTimes(1);
+      });
+
+      it('triggers saveCandle with expected parameters', () => {
+        expect(mockSaveCandle).toHaveBeenCalledWith(
+          loggerMock,
+          'trailing-trade-ath-candles',
+          {
+            close: 0.056324,
+            high: 0.056565,
+            interval: '1d',
+            key: 'ETHBTC',
+            low: 0.056132,
+            open: 0.05655,
+            time: 1508328900000,
+            volume: 68.888
+          }
+        );
+      });
+    });
+  });
+
+  describe('when ATH disabled', () => {
+    describe('setupATHCandlesWebsocket', () => {
+      beforeEach(async () => {
+        const { binance, logger } = require('../../helpers');
+        binanceMock = binance;
+        loggerMock = logger;
+
+        mockGetConfiguration = jest
+          .fn()
+          .mockImplementation((_logger, _symbol) => ({
+            buy: {
+              athRestriction: {
+                enabled: false,
+                candles: { interval: '1d' }
+              }
+            }
+          }));
+
+        mockSaveCandle = jest.fn().mockResolvedValue(true);
+
+        mockWebsocketATHCandlesClean = jest.fn();
+
+        binanceMock.client.ws.candles = jest.fn().mockResolvedValue(true);
+
+        jest.mock('../../cronjob/trailingTradeHelper/common', () => ({
+          saveCandle: mockSaveCandle
+        }));
+
+        jest.mock('../../cronjob/trailingTradeHelper/configuration', () => ({
+          getConfiguration: mockGetConfiguration
+        }));
+
+        athCandles = require('../ath-candles');
+
+        await athCandles.setupATHCandlesWebsocket(loggerMock, ['ETHBTC']);
+      });
+
+      it('triggers getConfiguration', () => {
+        expect(mockGetConfiguration).toHaveBeenCalledWith(loggerMock, 'ETHBTC');
+      });
+
+      it('does not trigger saveCandle', () => {
+        expect(mockSaveCandle).not.toHaveBeenCalled();
+      });
+
+      it('does not trigger binance.client.ws.candles', () => {
+        expect(binanceMock.client.ws.candles).not.toHaveBeenCalled();
+      });
+
+      it('checks websocketATHCandlesClean', () => {
+        expect(athCandles.getWebsocketATHCandlesClean()).toStrictEqual({});
+      });
+
+      it('does not trigger websocketATHCandlesClean', () => {
+        expect(mockWebsocketATHCandlesClean).not.toHaveBeenCalled();
+      });
+    });
+
+    describe('syncATHCandles', () => {
+      beforeEach(async () => {
+        const { binance, logger, mongo } = require('../../helpers');
+        binanceMock = binance;
+        loggerMock = logger;
+        mongoMock = mongo;
+
+        mockGetConfiguration = jest
+          .fn()
+          .mockImplementation((_logger, _symbol) => ({
+            buy: {
+              athRestriction: {
+                enabled: false,
+                candles: { interval: '1d', limit: 1 }
+              }
+            }
+          }));
+
+        mockSaveCandle = jest.fn().mockResolvedValue(true);
+
+        mongoMock.deleteAll = jest.fn().mockResolvedValue(true);
+
+        binanceMock.client.candles = jest.fn().mockResolvedValue(true);
+
+        jest.mock('../../cronjob/trailingTradeHelper/common', () => ({
+          saveCandle: mockSaveCandle
+        }));
+
+        jest.mock('../../cronjob/trailingTradeHelper/configuration', () => ({
+          getConfiguration: mockGetConfiguration
+        }));
+
+        athCandles = require('../ath-candles');
+
+        await athCandles.syncATHCandles(loggerMock, ['ETHBTC']);
+      });
+
+      it('triggers mongo.deleteAll', () => {
+        expect(mongoMock.deleteAll).toHaveBeenCalledWith(
+          loggerMock,
+          'trailing-trade-ath-candles',
+          {
+            key: 'ETHBTC'
+          }
+        );
+      });
+
+      it('triggers getConfiguration', () => {
+        expect(mockGetConfiguration).toHaveBeenCalledWith(loggerMock, 'ETHBTC');
+      });
+
+      it('does not trigger binance.client.candles', () => {
+        expect(binanceMock.client.candles).not.toHaveBeenCalled();
+      });
+
+      it('does not trigger saveCandle', () => {
+        expect(mockSaveCandle).not.toHaveBeenCalled();
+      });
+    });
+  });
+});
---
+++ app/binance/__tests__/candles.test.js
@@ -0,0 +1,227 @@
+/* eslint-disable global-require */
+describe('candles.js', () => {
+  let candles;
+  let binanceMock;
+  let loggerMock;
+  let mongoMock;
+
+  let mockGetConfiguration;
+  let mockSaveCandle;
+
+  let mockWebsocketCandlesClean;
+
+  beforeEach(() => {
+    jest.clearAllMocks().resetModules();
+  });
+
+  describe('setupCandlesWebsocket', () => {
+    beforeEach(async () => {
+      const { binance, logger } = require('../../helpers');
+      binanceMock = binance;
+      loggerMock = logger;
+
+      mockGetConfiguration = jest
+        .fn()
+        .mockImplementation((_logger, _symbol) => ({
+          candles: { interval: '30m' }
+        }));
+
+      mockSaveCandle = jest.fn().mockResolvedValue(true);
+
+      mockWebsocketCandlesClean = jest.fn();
+
+      binanceMock.client.ws.candles = jest.fn((symbols, candleInterval, fn) => {
+        fn({
+          eventType: 'kline',
+          eventTime: 1525285576276,
+          symbol: 'ETHBTC',
+          startTime: 1525285560000,
+          open: '0.04898000',
+          high: '0.04902700',
+          low: '0.04898000',
+          close: '0.04901900',
+          closeTime: 1525285619999,
+          volume: '37.89600000',
+          trades: 30,
+          interval: '30m',
+          isFinal: false,
+          quoteVolume: '1.85728874',
+          buyVolume: '21.79900000',
+          quoteBuyVolume: '1.06838790'
+        });
+
+        return mockWebsocketCandlesClean;
+      });
+
+      jest.mock('../../cronjob/trailingTradeHelper/common', () => ({
+        saveCandle: mockSaveCandle
+      }));
+
+      jest.mock('../../cronjob/trailingTradeHelper/configuration', () => ({
+        getConfiguration: mockGetConfiguration
+      }));
+
+      candles = require('../candles');
+
+      await candles.setupCandlesWebsocket(loggerMock, ['ETHBTC', 'BNBUSDT']);
+    });
+
+    it('triggers getConfiguration twice', () => {
+      expect(mockGetConfiguration).toHaveBeenCalledTimes(2);
+    });
+
+    it('triggers getConfiguration for ETHBTC', () => {
+      expect(mockGetConfiguration).toHaveBeenCalledWith(loggerMock, 'ETHBTC');
+    });
+
+    it('triggers getConfiguration for BNBUSDT', () => {
+      expect(mockGetConfiguration).toHaveBeenCalledWith(loggerMock, 'BNBUSDT');
+    });
+
+    it('triggers saveCandle', () => {
+      expect(mockSaveCandle).toHaveBeenCalledWith(
+        loggerMock,
+        'trailing-trade-candles',
+        {
+          close: 0.049019,
+          high: 0.049027,
+          interval: '30m',
+          key: 'ETHBTC',
+          low: 0.04898,
+          open: 0.04898,
+          time: 1525285560000,
+          volume: 37.896
+        }
+      );
+    });
+
+    it('checks websocketCandlesClean', () => {
+      expect(candles.getWebsocketCandlesClean()).toStrictEqual({
+        '30m': expect.any(Function)
+      });
+    });
+
+    it('does not trigger websocketCandlesClean', () => {
+      expect(mockWebsocketCandlesClean).not.toHaveBeenCalled();
+    });
+
+    describe('when called again', () => {
+      beforeEach(async () => {
+        mockGetConfiguration.mockClear();
+        await candles.setupCandlesWebsocket(loggerMock, ['BTCUSDT']);
+      });
+
+      it('triggers getConfiguration twice', () => {
+        expect(mockGetConfiguration).toHaveBeenCalledTimes(1);
+      });
+
+      it('triggers getConfiguration for BTCUSDT', () => {
+        expect(mockGetConfiguration).toHaveBeenCalledWith(
+          loggerMock,
+          'BTCUSDT'
+        );
+      });
+
+      it('triggers websocketCandlesClean', () => {
+        expect(mockWebsocketCandlesClean).toHaveBeenCalledTimes(1);
+      });
+    });
+  });
+
+  describe('syncCandles', () => {
+    beforeEach(async () => {
+      const { binance, logger, mongo } = require('../../helpers');
+      binanceMock = binance;
+      loggerMock = logger;
+      mongoMock = mongo;
+
+      mockGetConfiguration = jest
+        .fn()
+        .mockImplementation((_logger, _symbol) => ({
+          candles: { interval: '30m', limit: 1 }
+        }));
+
+      mockSaveCandle = jest.fn().mockResolvedValue(true);
+
+      mongoMock.deleteAll = jest.fn().mockResolvedValue(true);
+
+      binanceMock.client.candles = jest.fn().mockImplementation(options => {
+        const { symbol } = options;
+
+        if (symbol === 'ETHBTC') {
+          return [
+            {
+              openTime: 1508328900000,
+              open: '0.05655000',
+              high: '0.05656500',
+              low: '0.05613200',
+              close: '0.05632400',
+              volume: '68.88800000',
+              closeTime: 1508329199999,
+              quoteAssetVolume: '2.29500857',
+              trades: 85,
+              baseAssetVolume: '40.61900000'
+            }
+          ];
+        }
+
+        return [];
+      });
+
+      jest.mock('../../cronjob/trailingTradeHelper/common', () => ({
+        saveCandle: mockSaveCandle
+      }));
+
+      jest.mock('../../cronjob/trailingTradeHelper/configuration', () => ({
+        getConfiguration: mockGetConfiguration
+      }));
+
+      candles = require('../candles');
+
+      await candles.syncCandles(loggerMock, ['ETHBTC']);
+    });
+
+    it('triggers mongo.deleteAll', () => {
+      expect(mongoMock.deleteAll).toHaveBeenCalledWith(
+        loggerMock,
+        'trailing-trade-candles',
+        {
+          key: 'ETHBTC'
+        }
+      );
+    });
+
+    it('triggers getConfiguration for ETHBTC', () => {
+      expect(mockGetConfiguration).toHaveBeenCalledWith(loggerMock, 'ETHBTC');
+    });
+
+    it('triggers binance.client.candles', () => {
+      expect(binanceMock.client.candles).toHaveBeenCalledWith({
+        interval: '30m',
+        limit: 1,
+        symbol: 'ETHBTC'
+      });
+    });
+
+    it('triggers saveCandle one time', () => {
+      expect(mockSaveCandle).toHaveBeenCalledTimes(1);
+    });
+
+    it('triggers saveCandle with expected parameters', () => {
+      expect(mockSaveCandle).toHaveBeenCalledWith(
+        loggerMock,
+        'trailing-trade-candles',
+        {
+          close: 0.056324,
+          high: 0.056565,
+          interval: '30m',
+          key: 'ETHBTC',
+          low: 0.056132,
+          open: 0.05655,
+          time: 1508328900000,
+          volume: 68.888
+        }
+      );
+    });
+  });
+});
