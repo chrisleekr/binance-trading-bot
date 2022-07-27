0c78164889a20c0989b020ce1205ee003471c15d
uhliksk <uhlik@uhlik.sk>
2022-07-27 13:37:07 +0200

    fix: better pagination

--- public/js/App.js
+++ public/js/App.js
@@ -349,28 +349,19 @@ class App extends React.Component {
         disabled={page === 1 || totalPages === 1}
       />
     );
-    [...Array(3).keys()].forEach(index => {
-      if (page === 1 && index === 0) {
-        paginationItems.push(
-          <Pagination.Item
-            active
-            key={`pagination-item-${index}`}
-            onClick={() => this.setPage(page)}>
-            {page}
-          </Pagination.Item>
-        );
-      } else {
-        const pageNum = page === 1 ? page + index : page + index - 1;
-        paginationItems.push(
-          <Pagination.Item
-            active={pageNum === page}
-            disabled={pageNum > totalPages}
-            key={`pagination-item-${index}`}
-            onClick={() => this.setPage(pageNum)}>
-            {pageNum}
-          </Pagination.Item>
-        );
-      }
+    const maxButtons = 8;
+    const buttons = Math.min( maxButtons , ~~totalPages );
+    [...Array(buttons).keys()].forEach(x => {
+      const pageNum = Math.min( Math.max( x + 1 , page + x + 1 - Math.ceil( buttons / 2 ) ) , totalPages + x + 1 - buttons );
+      paginationItems.push(
+        <Pagination.Item
+          active={pageNum === page}
+          disabled={pageNum > totalPages}
+          key={`pagination-item-${x}`}
+          onClick={() => this.setPage(pageNum)}>
+          {pageNum}
+        </Pagination.Item>
+      );
     });
     paginationItems.push(
       <Pagination.Next
