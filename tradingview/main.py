0668b9aeb9439938536d8769caf69311e7a5e5b6
rando128 <rando128@hotmail.com>
2023-03-20 12:48:27 +0100

    chore: add docker host and port configurability of tradingview server (#608)

--- tradingview/main.py
+++ tradingview/main.py
@@ -51,4 +51,6 @@ def index():
 
 if __name__ == "__main__":
     from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
+
+    port = os.environ.get("TRADINGVIEW_PORT", 8080)
+    serve(app, host="0.0.0.0", port=port)
