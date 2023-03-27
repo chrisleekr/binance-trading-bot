880c7d5cd453f776e59ad22ff7314a734319133c
Chris Lee <git@chrislee.kr>
2023-03-28 08:17:15 +1100

    chore: changed TRADINGVIEW_LOG_LEVEL as well

--- tradingview/main.py
+++ tradingview/main.py
@@ -9,7 +9,7 @@ from tradingview_ta import get_multiple_analysis
 app = Flask(__name__)
 
 logger = logging.getLogger('')
-logger.setLevel(os.environ.get("TRADINGVIEW_LOG_LEVEL", logging.DEBUG))
+logger.setLevel(os.environ.get("BINANCE_TRADINGVIEW_LOG_LEVEL", logging.DEBUG))
 sh = logging.StreamHandler(sys.stdout)
 sh.setFormatter(colorlog.ColoredFormatter(
     '%(log_color)s [%(asctime)s] %(levelname)s [%(filename)s.%(funcName)s:%(lineno)d] %(message)s', datefmt='%a, %d %b %Y %H:%M:%S'))
